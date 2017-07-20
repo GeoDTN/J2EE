25ee8eb971dc8201df284f07567418af1172310c
Tadewos Somano <tadewos85@gmail.com>
2017-07-20 09:42:21 +0200

    Create ReadXML.java

---
+++ ReadXML.java
@@ -0,0 +1,38 @@
+package org.xtphere.example1;
+
+import java.io.File;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+
+public class ReadXML {
+
+	public static void main(String[] args) {
+		
+
+			  try {
+				  File fXmlFile = new File("/Users/mkyong/staff.xml");
+					DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+					DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+					Document doc = dBuilder.parse(fXmlFile);
+
+					//optional, but recommended
+					//read this - http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
+					doc.getDocumentElement().normalize();
+
+					System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+
+					NodeList nList = doc.getElementsByTagName("staff");
+
+					System.out.println("----------------------------");
+			  } catch (Exception pce) {
+				pce.printStackTrace();
+			  }
+			  	
+		
+	}
+
+}
